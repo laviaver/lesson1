61ccaacf581aecdb71be860ea0434ae1cd2d6026
laviaver <laviaver@gmail.com>
2026-05-25 07:24:04 +0300

    Zod update

--- routes/employees.js
+++ routes/employees.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { writeLimiter } = require("../middleware/rateLimiter");
+const validate = require("../middleware/validate");
+const {
+  createEmployeeSchema,
+  updateEmployeeSchema,
+} = require("../validators/employeeValidator");
 
 const {
   getEmployees,
   getEmployee,
   createEmployee,
   deleteEmployee,
-  updateEmployee
+  updateEmployee,
 } = require("../controllers/employeesController");
-
-const validateUpdateEmployee = require("../middleware/validateUpdateEmployee");
-
-const validateCreateEmployee = require("../middleware/validateCreateEmployee");
 
 // Routes
 router.get("/", getEmployees);
 router.get("/:id", getEmployee);
-router.post("/", writeLimiter, validateCreateEmployee, createEmployee);
-router.put("/:id", writeLimiter, validateUpdateEmployee, updateEmployee);
+router.post("/", writeLimiter, validate(createEmployeeSchema), createEmployee);
+router.put("/:id", writeLimiter, validate(updateEmployeeSchema), updateEmployee);
 router.delete("/:id", writeLimiter, deleteEmployee);
 
 module.exports = router;
